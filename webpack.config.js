9e1d721863496f4c7db36adb43378111db71ba8f
Your Name <futunplay@gmail.com>
2021-02-13 22:13:52 +0500

    new organization code

--- webpack.config.js
+++ webpack.config.js
@@ -1,78 +1,100 @@
 const path = require("path");
 const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
 
-module.exports = {
-  target: ['web', 'es3'],
-  entry: "./src/App.tsx",
-  mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        // loader:
-        use: [
-          'ts-loader',
-          //  "babel-loader",
-        ]
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      },
-      {
-        test: /\.(ttf|woff|woff2|eot)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]',
-          outputPath: 'fonts',
-          publicPath: "./dist/fonts"
-        }
-      },
-      {
-        test: /\.(svg|jpeg|jpg)$/,
-        include: [path.join(__dirname, "src/assets")],
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]',
-          outputPath: 'img',
-          publicPath: "./dist/img"
-        }
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          // Creates `style` nodes from JS strings
-          { loader: 'style-loader' },
-          // {
-          //     loader: MiniCssExtractPlugin.loader,
-          // },
-          // Translates CSS into CommonJS
-          { loader: 'css-loader' },
-          // { loader: 'postcss-loader' },
-          // { loader: 'resolve-url-loader' },
-          {
-            // Compiles Sass to CSS
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true
-            }
+let rootDirectory = '';
+
+module.exports = (env, options) => {
+  if (options.mode === 'production')
+    rootDirectory = '/FSD';
+
+  return {
+    target: ['web', 'es3'],
+    entry: "./src/App.tsx",
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          extractComments: false,
+        }),
+      ],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          exclude: /(node_modules|bower_components)/,
+          // loader:
+          use: [
+            'ts-loader',
+            //  "babel-loader",
+          ]
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"]
+        },
+        {
+          test: /\.(ttf|woff|woff2|eot)$/,
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts',
+            publicPath: rootDirectory + "/fonts"
           }
-        ]
-      },
+        },
+        {
+          test: /\.(svg|jpeg|jpg)$/,
+          include: [path.join(__dirname, "src/assets")],
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'img',
+            publicPath: rootDirectory + "/img"
+          }
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            // Creates `style` nodes from JS strings
+            { loader: 'style-loader' },
+            // {
+            //     loader: MiniCssExtractPlugin.loader,
+            // },
+            // Translates CSS into CommonJS
+            { loader: 'css-loader' },
+            // { loader: 'postcss-loader' },
+            // { loader: 'resolve-url-loader' },
+            {
+              // Compiles Sass to CSS
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        },
+      ]
+    },
+    resolve: { extensions: ["*", ".ts", ".tsx", '.js'] },
+    output: {
+      path: path.resolve(__dirname, "dist/"),
+      publicPath: rootDirectory,
+      filename: "bundle.js"
+    },
+    devServer: {
+      contentBase: path.join(__dirname, "public/"),
+      port: 3000,
+      publicPath: "http://localhost:3000/",
+      hotOnly: false,
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+        // favicon: "./public/favicon.ico"
+      }),
     ]
-  },
-  resolve: { extensions: ["*", ".ts", ".tsx", '.js'] },
-  output: {
-    path: path.resolve(__dirname, "public/dist/"),
-    publicPath: "/dist/",
-    filename: "bundle.js"
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "public/"),
-    port: 3000,
-    publicPath: "http://localhost:3000/dist/",
-    hotOnly: true
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
-};
+  }
+}
